81ca7a19389efab5125bf9fc1109f02c88d77d29
sttcha31 <sttcha31@gmail.com>
2024-10-25 12:37:08 -0400

    hi

--- betmgm_scraper.py
+++ betmgm_scraper.py
@@ -15,23 +15,26 @@ op = webdriver.ChromeOptions()
 op.add_argument("--enable-features=SameSiteByDefaultCookies,CookiesWithoutSameSiteMustBeSecure")
 op.add_argument("--enable-javascript")
 
+def get_game_links():
+    driver = webdriver.Chrome(options=op)
+    driver.get("https://sports.mi.betmgm.com/en/sports/basketball-7/betting/usa-9/nba-6004")
+    wait = WebDriverWait(driver, 20)
+    #CHECK: following line's XPATH might chance from day to day
+    table = wait.until(EC.presence_of_element_located((By.XPATH, './/*[@id="main-view"]/ms-widget-layout/ms-widget-slot/ms-composable-widget/ms-widget-slot/ms-tabbed-grid-widget/ms-grid/div/ms-event-group')))
+    games = table.find_elements(By.XPATH, "./*")
+    with open("sample.txt", "w") as file: 
+        for game in games:
+            file.write(game.find_element(By.CLASS_NAME, "grid-event-wrapper").find_element(By.TAG_NAME, "a").get_attribute("href") + "\n")
 
-
-user_agent_list = {
-	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36', 
-
-}
-
-#/html/body/div[2]/div[2]/section/section[2]/section/div[2]/div/section/div[4]/div/div/div[2]/div/div
-def get_player_point_overunder(link):
+def get_player_overunder(link, category):
     overunders = {}
     driver = webdriver.Chrome(options=op)
-    driver.get(link)
-    time.sleep(3)
-    driver.find_element(By.XPATH, './/*[@id="main-view"]/ng-component/div/ms-option-group-list/div[1]/ms-option-panel[1]/div/ms-player-props-option-group/ms-option-panel-bottom-action/div').click()
+    driver.get(link+":"+category)
+    wait = WebDriverWait(driver, 20)
+    button = wait.until(EC.presence_of_element_located((By.XPATH, './/*[@id="main-view"]/ng-component/div/ms-option-group-list/div[1]/ms-option-panel[1]/div/ms-player-props-option-group/ms-option-panel-bottom-action/div')))
+    button.click()
     table = driver.find_element(By.XPATH, '//*[@id="main-view"]/ng-component/div/ms-option-group-list/div[1]/ms-option-panel[1]/div/ms-player-props-option-group/ms-split-header/div')
     content = table.find_elements(By.XPATH, "./*")
-    
     for i in range(1, len(content), 3):
         playername = content[i].find_element(By.CLASS_NAME, "player-props-player-name").text
         points = content[i+1].find_element(By.CLASS_NAME, "option-pick").find_element(By.CLASS_NAME, "option-indicator").find_element(By.CLASS_NAME, "name").text[5:]
@@ -39,8 +42,18 @@ def get_player_point_overunder(link):
         under = content[i+2].find_element(By.CLASS_NAME, "option-pick").find_element(By.CLASS_NAME, "option-indicator").find_element(By.CLASS_NAME, "value").find_element(By.TAG_NAME, 'ms-font-resizer').find_element(By.CLASS_NAME, "custom-odds-value-style").text
         overunders[playername] = (points, over, under)
 
+    print(overunders)
+    return overunders
 
+def write_to_csv(overunders):
     return
-    # return
 
-get_player_point_overunder('https://sports.mi.betmgm.com/en/sports/events/boston-celtics-at-washington-wizards-16243435?market=Players:Points&tab=score')
+
+# get_player_overunder('https://sports.mi.betmgm.com/en/sports/events/brooklyn-nets-at-orlando-magic-16517249?tab=score&market=Players', "Points")
+# get_player_overunder('https://sports.mi.betmgm.com/en/sports/events/brooklyn-nets-at-orlando-magic-16517249?tab=score&market=Players', "Rebounds")
+# get_player_overunder('https://sports.mi.betmgm.com/en/sports/events/brooklyn-nets-at-orlando-magic-16517249?tab=score&market=Players', "Assists")
+# get_player_overunder('https://sports.mi.betmgm.com/en/sports/events/brooklyn-nets-at-orlando-magic-16517249?tab=score&market=Players', "3-Pointers")
+
+get_game_links()
+
+
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "Bookie.hpp"
+#include <Python.h>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    Py_Initialize();
+
+    PyEval_InitThreads();
+
+    int numthreads;
+
+}
