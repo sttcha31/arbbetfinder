f1fffb14bc44b22237169279fdd1253e1da66529
sttcha31 <sttcha31@gmail.com>
2024-11-24 18:36:21 -0500

    progress

--- betmgm_scraper.py
+++ betmgm_scraper.py
@@ -20,6 +20,7 @@ def get_game_links():
     driver.get("https://sports.mi.betmgm.com/en/sports/basketball-7/betting/usa-9/nba-6004")
     wait = WebDriverWait(driver, 20)
     #CHECK: following line's XPATH might chance from day to day
+                                                                
     table = wait.until(EC.presence_of_element_located((By.XPATH, './/*[@id="main-view"]/ms-widget-layout/ms-widget-slot/ms-composable-widget/ms-widget-slot/ms-tabbed-grid-widget/ms-grid/div/ms-event-group')))
     games = table.find_elements(By.XPATH, "./*")
     with open("mgmgames.txt", "w") as file: 
@@ -29,17 +30,20 @@ def get_game_links():
 def get_player_overunder(link, category):
     overunders = {}
     driver = webdriver.Chrome(options=op)
-    driver.get(link+":"+category)
+    driver.get(link+"?market=Players:"+category)
     wait = WebDriverWait(driver, 20)
     button = wait.until(EC.presence_of_element_located((By.XPATH, './/*[@id="main-view"]/ng-component/div/ms-option-group-list/div[1]/ms-option-panel[1]/div/ms-player-props-option-group/ms-option-panel-bottom-action/div')))
     button.click()
+
     table = driver.find_element(By.XPATH, '//*[@id="main-view"]/ng-component/div/ms-option-group-list/div[1]/ms-option-panel[1]/div/ms-player-props-option-group/ms-split-header/div')
+    print(table)
+
     content = table.find_elements(By.XPATH, "./*")
-    for i in range(1, len(content), 3):
+    for i in range(1, len(content)):
         playername = content[i].find_element(By.CLASS_NAME, "player-props-player-name").text
-        points = content[i+1].find_element(By.CLASS_NAME, "option-pick").find_element(By.CLASS_NAME, "option-indicator").find_element(By.CLASS_NAME, "name").text[5:]
-        over = content[i+1].find_element(By.CLASS_NAME, "option-pick").find_element(By.CLASS_NAME, "option-indicator").find_element(By.CLASS_NAME, "value").find_element(By.TAG_NAME, 'ms-font-resizer').find_element(By.CLASS_NAME, "custom-odds-value-style").text
-        under = content[i+2].find_element(By.CLASS_NAME, "option-pick").find_element(By.CLASS_NAME, "option-indicator").find_element(By.CLASS_NAME, "value").find_element(By.TAG_NAME, 'ms-font-resizer').find_element(By.CLASS_NAME, "custom-odds-value-style").text
+        points = content[i].find_element(By.CLASS_NAME, "option-pick").find_element(By.CLASS_NAME, "option-indicator").find_element(By.CLASS_NAME, "name").text[2:]
+        over = content[i].find_element(By.CLASS_NAME, "option-pick").find_element(By.CLASS_NAME, "option-indicator").find_element(By.CLASS_NAME, "value").find_element(By.TAG_NAME, 'ms-font-resizer').find_element(By.CLASS_NAME, "custom-odds-value-style").text
+        under = content[i].find_element(By.CLASS_NAME, "option-pick").find_element(By.CLASS_NAME, "option-indicator").find_element(By.CLASS_NAME, "value").find_element(By.TAG_NAME, 'ms-font-resizer').find_element(By.CLASS_NAME, "custom-odds-value-style").text
         overunders[playername] = (points, over, under)
 
     print(overunders)
@@ -49,7 +53,7 @@ def write_to_csv(overunders):
     return
 
 
-# get_player_overunder('https://sports.mi.betmgm.com/en/sports/events/brooklyn-nets-at-orlando-magic-16517249?tab=score&market=Players', "Points")
+# get_player_overunder('https://sports.mi.betmgm.com/en/sports/events/minnesota-timberwolves-at-boston-celtics-16529704', "Points")
 # get_player_overunder('https://sports.mi.betmgm.com/en/sports/events/brooklyn-nets-at-orlando-magic-16517249?tab=score&market=Players', "Rebounds")
 # get_player_overunder('https://sports.mi.betmgm.com/en/sports/events/brooklyn-nets-at-orlando-magic-16517249?tab=score&market=Players', "Assists")
 # get_player_overunder('https://sports.mi.betmgm.com/en/sports/events/brooklyn-nets-at-orlando-magic-16517249?tab=score&market=Players', "3-Pointers")
--- main.cpp
+++ main.cpp
@@ -4,10 +4,135 @@
 #include <Python.h>
 #include <fstream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
+// class ArbFinder {
+//     public:
+//         ArbFinder() {}
 
-void call_python_function(const string& module_name, const string& function_name) {
+//         void call_python_function(const string& module_name, const string& function_name, vector<string> arguments) {
+//             PyObject* pModule = PyImport_ImportModule(module_name.c_str());
+//             if (pModule == nullptr) {
+//                 PyErr_Print();
+//                 return;
+//             }
+//             PyObject* pFunc = PyObject_GetAttrString(pModule, function_name.c_str());
+//             if (pFunc == nullptr) {
+//                 PyErr_Print();
+//                 return;
+//             }
+//             PyObject* pResult;
+//             PyObject* pArgs = PyTuple_New(arguments.size());
+//             for(int i = 0; i < arguments.size(); ++i){
+//                 PyTuple_SetItem(pArgs, i, PyUnicode_FromString(arguments[i].c_str()));
+//             }
+//             pResult = PyObject_CallObject(pFunc, pArgs);
+//             if (pResult == nullptr) {
+//                 PyErr_Print();
+//                 return;
+//             }
+//             PyObject* keys = PyDict_Keys(pResult);
+//             if (keys != nullptr) {
+//                 Py_ssize_t size = PyList_Size(keys); // Number of items in the dictionary
+//                 for (Py_ssize_t i = 0; i < size; ++i) {
+//                     // Get key and value
+//                     PyObject* key = PyList_GetItem(keys, i);
+//                     PyObject* value = PyDict_GetItem(pResult, key);
+
+//                     // Convert the key to a string
+//                     const char* keyStr = PyUnicode_AsUTF8(key);
+//                     std::cout << keyStr << ": ";
+
+//                     // Check if the value is a tuple
+//                     if (PyTuple_Check(value)) {
+//                         std::cout << "(";
+//                         Py_ssize_t tupleSize = PyTuple_Size(value);
+//                         for (Py_ssize_t j = 0; j < tupleSize; ++j) {
+//                             PyObject* item = PyTuple_GetItem(value, j);
+//                             if (PyUnicode_Check(item)) {
+//                                 const char* itemStr = PyUnicode_AsUTF8(item);
+//                                 std::cout << itemStr;
+//                             }
+//                             if (j < tupleSize - 1) std::cout << ", ";
+//                         }
+//                         std::cout << ")";
+//                     } else {
+//                         std::cout << "(unexpected type)";
+//                     }
+//                     std::cout << std::endl;
+//                 }
+//             }
+//             Py_DECREF(pResult);
+//             Py_DECREF(pFunc);
+//             Py_DECREF(pModule);
+//         }
+//     private:
+//         unordered_map<pair<string, string>, vector<Bookie*>> hashMap;
+
+// };
+
+void call_python_function(const string& module_name, const string& function_name, vector<string> arguments) {
+    PyGILState_STATE gstate = PyGILState_Ensure();
+
+    PyObject* pModule = PyImport_ImportModule(module_name.c_str());
+    if (pModule == nullptr) {
+        PyErr_Print();
+        return;
+    }
+    PyObject* pFunc = PyObject_GetAttrString(pModule, function_name.c_str());
+    if (pFunc == nullptr) {
+        PyErr_Print();
+        return;
+    }
+    PyObject* pResult;
+    PyObject* pArgs = PyTuple_New(arguments.size());
+    for(int i = 0; i < arguments.size(); ++i){
+        PyTuple_SetItem(pArgs, i, PyUnicode_FromString(arguments[i].c_str()));
+    }
+    pResult = PyObject_CallObject(pFunc, pArgs);
+    if (pResult == nullptr) {
+        PyErr_Print();
+        return;
+    }
+    PyObject* keys = PyDict_Keys(pResult);
+    if (keys != nullptr) {
+        Py_ssize_t size = PyList_Size(keys); // Number of items in the dictionary
+        for (Py_ssize_t i = 0; i < size; ++i) {
+            // Get key and value
+            PyObject* key = PyList_GetItem(keys, i);
+            PyObject* value = PyDict_GetItem(pResult, key);
+
+            // Convert the key to a string
+            const char* keyStr = PyUnicode_AsUTF8(key);
+            std::cout << keyStr << ": ";
+
+            // Check if the value is a tuple
+            if (PyTuple_Check(value)) {
+                std::cout << "(";
+                Py_ssize_t tupleSize = PyTuple_Size(value);
+                for (Py_ssize_t j = 0; j < tupleSize; ++j) {
+                    PyObject* item = PyTuple_GetItem(value, j);
+                    if (PyUnicode_Check(item)) {
+                        const char* itemStr = PyUnicode_AsUTF8(item);
+                        std::cout << itemStr;
+                    }
+                    if (j < tupleSize - 1) std::cout << ", ";
+                }
+                std::cout << ")";
+            } else {
+                std::cout << "(unexpected type)";
+            }
+            std::cout << std::endl;
+        }
+    }
+    Py_DECREF(pResult);
+    Py_DECREF(pFunc);
+    Py_DECREF(pModule);
+    PyGILState_Release(gstate); 
+}
+
+void get_links(const string& module_name, const string& function_name) {
      PyObject* pModule = PyImport_ImportModule(module_name.c_str());
     if (pModule == nullptr) {
         PyErr_Print();
@@ -29,64 +154,34 @@ void call_python_function(const string& module_name, const string& function_name
 }
 
 int main() {
-    // Py_Initialize();
 
-    // vector<thread> threads;
-    // ifstream fin("mgmgames.txt");
-    // string line;
-    // if(fin.is_open()) {
-    //     while (getline(fin, line)) {
-    //         cout << line << endl;
-    //         threads.push_back(thread(call_python_function, ""));
-    //     }
-    //     fin.close(); 
-    // }
-
-    // for (auto& th : threads) {
-    //     th.join();
-    // }
-    // Py_Finalize(); 
-    // return 0;
     Py_Initialize();
 
     PyObject* sysPath = PySys_GetObject("path");
     PyObject* currentDir = PyUnicode_FromString(".");
     PyList_Append(sysPath, currentDir);
     Py_DECREF(currentDir);
-    call_python_function("betmgm_scraper", "get_game_links");
-    // // Import the Python module
-    // PyObject* pModule = PyImport_ImportModule("my_module");
-    // if (pModule == nullptr) {
-    //     PyErr_Print();
-    //     return 1;
+    // vector<string> args = {"https://sports.mi.betmgm.com/en/sports/events/minnesota-timberwolves-at-boston-celtics-16529704", "points"};
+    // call_python_function("betmgm_scraper", "get_player_overunder", args);
+
+    vector<thread> threads;
+    ifstream fin("mgmgames.txt");
+    string line;
+    if(fin.is_open()) {
+        while (getline(fin, line)) {
+            cout << line << endl;
+            vector<string> args = {line, "points"};
+            call_python_function("betmgm_scraper", "get_player_overunder", args);
+            // threads.push_back(thread(call_python_function, "betmgm_scraper", "get_player_overunder", args));
+        }
+        fin.close(); 
+    }
+
+    // for (auto& th : threads) {
+    //     cout << "hi" << endl;
+    //     th.join();
     // }
 
-    // // Get the function from the module
-    // PyObject* pFunc = PyObject_GetAttrString(pModule, "my_function");
-    // if (pFunc == nullptr) {
-    //     PyErr_Print();
-    //     return 1;
-    // }
-
-    // // Call the function with arguments
-    // PyObject* pArgs = PyTuple_New(2);
-    // PyTuple_SetItem(pArgs, 0, PyLong_FromLong(10));
-    // PyTuple_SetItem(pArgs, 1, PyLong_FromLong(20));
-
-    // PyObject* pResult = PyObject_CallObject(pFunc, pArgs);
-    // if (pResult == nullptr) {
-    //     PyErr_Print();
-    //     return 1;
-    // }
-
-    // // Convert the result to a C++ type
-    // long result = PyLong_AsLong(pResult);
-    // std::cout << "Result: " << result << std::endl;
-    // // Clean up
-    // Py_DECREF(pArgs);
-    // Py_DECREF(pResult);
-    // Py_DECREF(pFunc);
-    // Py_DECREF(pModule);
     Py_Finalize();
 
     return 0;
